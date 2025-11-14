0333b86ead4a04098e5a08eb639648b9bdfc37ba
mcaswen <mcaswen@gmail.com>
2025-11-14 19:04:30 +0800

    feat: Implemented camp and Ani attributes components

---
+++ Assets/Scripts/Anis/Attributes/AniAttributesComponents.cs
@@ -0,0 +1,38 @@
+using Unity.Entities;
+using Unity.NetCode;
+
+[GhostComponent]
+public struct AniAttributes : IComponentData
+{
+    [GhostField]
+    public int MaxHealth;
+
+    [GhostField]
+    public int CurrentHealth;
+
+    [GhostField]
+    public float MoveSpeed;
+
+    public float AttackInterval;
+
+    public float AttackDamage;
+
+    public float AttackRange;
+
+    [GhostField]
+    public bool IsSelected;
+
+    [GhostField]
+    public int OwnerPlayerId;
+}
+public enum PickerAniTargetType
+{
+    Resource = 0,
+    Enemy = 1,
+}
+
+public struct PickerAniAttributes : IComponentData
+{
+    public float CarrySpeed;
+    public PickerAniTargetType TargetType;
+}
